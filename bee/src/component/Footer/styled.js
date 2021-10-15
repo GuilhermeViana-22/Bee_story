e8ba8c9cbe45848f2017eca4390779923425b082
Guilherme Viana <gguicido.viana@gmail.com>
2021-10-15 17:10:37 -0300

    ajuste footer

--- bee/src/component/Footer/styled.js
+++ bee/src/component/Footer/styled.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const AreaFooter = styled.div`
 
 background-color: #000;
-height:30px;  
+height:50px;  
 margin-top 30px;
 width:100%;
 text-align: center;
